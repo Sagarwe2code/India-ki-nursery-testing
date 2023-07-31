5dfea783cc1a83771788bf09fe95232f4f8a42c1
shivaniwe2code <141002722+Sagarwe2code@users.noreply.github.com>
2023-07-31 15:57:42 +0530

    Update Customer_application.java

--- India_ki_nursery/Customer_application.java
+++ India_ki_nursery/Customer_application.java
@@ -1,11 +1,14 @@
 package India_ki_nursery;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -55,7 +58,7 @@ public class Customer_application {
 		Thread.sleep(waiting_time);
 		WebElement lastname=driver.findElement(By.xpath("//*[@id=\"main_body\"]/div[3]/div/div/div/form/div[4]/input"));
 		lastname.clear();
-		lastname.sendKeys("Ram");
+		lastname.sendKeys("Singh");
 		Thread.sleep(waiting_time);
 		WebElement Contact=driver.findElement(By.xpath("//*[@id=\"main_body\"]/div[3]/div/div/div/form/div[5]/input"));
 		Contact.clear();
@@ -63,7 +66,7 @@ public class Customer_application {
 		Thread.sleep(waiting_time);
 		WebElement address=driver.findElement(By.xpath("//*[@id=\"main_body\"]/div[3]/div/div/div/form/div[6]/input"));
 		address.clear();
-		address.sendKeys("#107,4th phase vijay nagar indore");
+		address.sendKeys("2nd Floor, 45 Universal Tower, P. U. 4, Scheme 54 PU4, Indore, Madhya Pradesh 452001");
 		Thread.sleep(waiting_time);
 		WebElement email=driver.findElement(By.xpath("//*[@id=\"main_body\"]/div[3]/div/div/div/form/div[8]/input"));
 		email.clear();
@@ -80,49 +83,63 @@ public class Customer_application {
 		WebElement saveProfile=driver.findElement(By.xpath("//*[@id=\"main_body\"]/div[3]/div/div/div/form/button"));
 		saveProfile.click();
 		Thread.sleep(waiting_time);
-		Customer_application obj=new Customer_application();
-		obj.home(driver,waiting_time);
-	}
-	public void home(WebDriver driver,int waiting_time) throws InterruptedException {
-		Thread.sleep(waiting_time);
-		JavascriptExecutor js1=(JavascriptExecutor) driver;
-		js1.executeScript("window.scrollBy(0,-500)");
+		js.executeScript("window.scrollBy(0,-500)");
 		WebElement home=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[2]/div/div/div/div/ul/li[1]/a"));
 		home.click();
 		Thread.sleep(waiting_time);
+		Customer_application obj=new Customer_application();
+		obj.shop(driver,waiting_time);
+		//		obj.home(driver,waiting_time);
+	}
+	//	public void home(WebDriver driver,int waiting_time) throws InterruptedException {
+	//		JavascriptExecutor js=(JavascriptExecutor) driver;
+	//		js.executeScript("window.scrollBy(0,500)");
+	//		Thread.sleep(waiting_time);
+	//		WebElement product=driver.findElement(By.xpath("//*[@id=\"trending\"]/div/div[2]/div[1]/div"));
+	//		System.out.println("product is present");
+	//		Thread.sleep(waiting_time);
+	//		WebElement add_to_basket=driver.findElement(By.xpath("//*[@id=\"trending\"]/div/div[2]/div[1]/div/div[2]/button"));
+	//		add_to_basket.click();
+	//		System.out.println("The product added to cart.");
+	//		Thread.sleep(waiting_time+3000);
+	//		WebElement cart=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[3]/div/button/i"));
+	//		cart.click();
+	//		Thread.sleep(waiting_time);
+	//		//		int number_of_product_in_list=driver.findElements(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[1]/div/ul/a")).size();
+	//		//		for (int i=1;i<=number_of_product_in_list;i++) {
+	//		//			WebElement product0=driver.findElement(By.xpath("//*[@id=\\\"root\\\"]/div/div[1]/nav[1]/div/div[1]/div/ul/a["+i+"]"));
+	//		//			WebElement price=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[1]/div/ul/a[1]/div/div[2]/div[2]/h6"));
+	//		//			System.out.println(price.getText());
+	//		//		}
+	//		WebElement proceed_to_checkout=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[1]/div/div[2]/a"));
+	//		proceed_to_checkout.click();
+	//		Thread.sleep(waiting_time);
+	//		js.executeScript("window.scrollBy(0,1000)");
+	//		//			js.executeScript("window.scrollBy(500,600)");
+	//		//			Thread.sleep(waiting_time);
+	//		//			WebElement payment_option=driver.findElement(By.xpath("//*[@id=\"area\"]"));
+	//		//			payment_option.click();
+	//		//			js.executeScript("window.scrollBy(00)");
+	//		//			WebElement term_and_Condition=driver.findElement(By.xpath("//*[@id=\"checkout-check\"]"));
+	//		//			term_and_Condition.click();
+	//		//			WebElement proceed_to_checkout1=driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/div/div/div[3]/div/div[4]/a"));
+	//		//			proceed_to_checkout1.click();
+	//		//			Thread.sleep(waiting_time+4000);
+	//		//			proceed_to_checkout1.click();
+	//	}
+	public void shop(WebDriver driver,int waiting_time) throws InterruptedException {
+		Thread.sleep(waiting_time);
+		WebElement shop=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[2]/div/div/div/div/ul/li[2]/a"));
+		shop.click();
+		Thread.sleep(waiting_time);
 		JavascriptExecutor js=(JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,500)");
-		Thread.sleep(waiting_time);
-		WebElement product=driver.findElement(By.xpath("//*[@id=\"trending\"]/div/div[2]/div[1]/div"));
-		System.out.println("product is present");
-		Thread.sleep(waiting_time);
-		WebElement add_to_basket=driver.findElement(By.xpath("//*[@id=\"trending\"]/div/div[2]/div[1]/div/div[2]/button"));
-		add_to_basket.click();
-		System.out.println("The product added to cart.");
-		Thread.sleep(waiting_time+3000);
-		WebElement cart=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[3]/div/button/i"));
-		cart.click();
-		Thread.sleep(waiting_time);
-		//		int number_of_product_in_list=driver.findElements(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[1]/div/ul/a")).size();
-		//		for (int i=1;i<=number_of_product_in_list;i++) {
-		//			WebElement product0=driver.findElement(By.xpath("//*[@id=\\\"root\\\"]/div/div[1]/nav[1]/div/div[1]/div/ul/a["+i+"]"));
-		//			WebElement price=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[1]/div/ul/a[1]/div/div[2]/div[2]/h6"));
-		//			System.out.println(price.getText());
-		//		}
-		WebElement proceed_to_checkout=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/nav[1]/div/div[1]/div/div[2]/a"));
-		proceed_to_checkout.click();
-		Thread.sleep(waiting_time);
-		js.executeScript("window.scrollBy(0,1000)");
-		//			js.executeScript("window.scrollBy(500,600)");
-		//			Thread.sleep(waiting_time);
-		//			WebElement payment_option=driver.findElement(By.xpath("//*[@id=\"area\"]"));
-		//			payment_option.click();
-		//			js.executeScript("window.scrollBy(00)");
-		//			WebElement term_and_Condition=driver.findElement(By.xpath("//*[@id=\"checkout-check\"]"));
-		//			term_and_Condition.click();
-		//			WebElement proceed_to_checkout1=driver.findElement(By.xpath("//*[@id=\"root\"]/div/section/div/div/div[3]/div/div[4]/a"));
-		//			proceed_to_checkout1.click();
-		//			Thread.sleep(waiting_time+4000);
-		//			proceed_to_checkout1.click();
+		Select showby=new Select(driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/section/div/div/div[2]/div[1]/div/div[2]/div[1]/select")));
+		List<WebElement>options=showby.getOptions();
+		System.out.println(options.size());
+		for(WebElement printoption:options) {
+			System.out.println(printoption.getText());
+		}
+		//		System.out.println(showby.getFirstSelectedOption());
 	}
 }
